17a887ade2a0c7850b3a08e63b51ee1cd0ffa0bb
Laurinha <48133541+lauraregis@users.noreply.github.com>
2019-04-12 14:45:08 -0300

    Add files via upload

---
+++ exe2.php
@@ -0,0 +1,10 @@
+<?php
+    print "Digite um número:\n";
+    $n1 = (float)fgets(STDIN);
+   
+    if($n1>0){
+        print "O numero é positivo.";       
+    }
+    else { 
+    print "O numero é negativo.";    
+}  
